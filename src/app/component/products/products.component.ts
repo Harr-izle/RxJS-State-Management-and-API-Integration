c22ba7248cf9332c9e9e0e8547f7c4bcf7e77260
Harr-izle <harrietbuadee@gmail.com>
2024-09-05 14:54:06 +0000

    adds api service for fetching the data

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrls: ['./products.component.css'] 
 })
 export class ProductsComponent {
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(){
+    this.apiService.getProducts().subscribe((data) => {
+      console.log(data);
+    })
+  }
+    
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../interface/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  apiUrl: string = 'https://api.escuelajs.co/api/v1/categories';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+
+  }
+}
